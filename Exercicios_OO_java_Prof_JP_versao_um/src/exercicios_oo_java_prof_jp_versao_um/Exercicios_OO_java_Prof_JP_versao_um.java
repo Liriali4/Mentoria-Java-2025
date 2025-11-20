e8c7342a573824b7c1f46f366685cd81555e93a8
Liriali4 <lirialia51@gmail.com>
2025-11-20 12:10:55 +0100

    Feat:Projecto da Lista de exercicios da Mentoria

---
+++ Exercicios_OO_java_Prof_JP_versao_um/src/exercicios_oo_java_prof_jp_versao_um/Exercicios_OO_java_Prof_JP_versao_um.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicios_oo_java_prof_jp_versao_um;
+
+/**
+ *
+ * @author hp
+ */
+public class Exercicios_OO_java_Prof_JP_versao_um {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
